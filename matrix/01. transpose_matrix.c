ae9f653c5d244ec4ad6425d52827d3d92d2f7cf4
Syed Abtahi Bin Mashak <syedabtahibinmashak@gmail.com>
2025-04-06 01:49:32 +0600

    ===

--- matrix/01. transpose_matrix.c
+++ matrix/01. transpose_matrix.c
@@ -6,13 +6,10 @@ int main()
     int mat[r][c]; printf("Enter the Elements :\n");
     for(int i=0; i<r; i++) for(int j=0; j<c; j++) scanf("%d",&mat[i][j]);
     
-    int tra[c][r];
-    for(int i=0; i<c; i++) for(int j=0; j<r; j++) tra[i][j]=mat[j][i];
-    
     printf("\nTranspose Matrix :\n");
     for(int i=0; i<c; i++)
     {
-        for(int j=0; j<r; j++) printf("%d ",tra[i][j]);
+        for(int j=0; j<r; j++) printf("%d ",mat[j][i]);
         printf("\n");
     }
     return 0;
